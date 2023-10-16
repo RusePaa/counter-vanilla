2dbcf64f30a6afb8fb341a6992f68b604c1b13e3
RusePaa <galanperezpablo@gmail.com>
2023-10-16 10:05:36 +0200

    Ejercicio Resuelto

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const counter = document.getElementById('numero');
+const decreaseButton = document.querySelector('.buttons .btn:nth-child(1)');
+const resetButton = document.querySelector('.buttons .btn:nth-child(2)');
+const increaseButton = document.querySelector('.buttons .btn:nth-child(3)');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+let count = 0;
 
-setupCounter(document.querySelector('#counter'))
+function decrease() {
+  count--;
+  counter.textContent = count;
+}
+
+function reset() {
+  count = 0;
+  counter.textContent = count;
+}
+
+function increase() {
+  count++;
+  counter.textContent = count;
+}
+
+decreaseButton.addEventListener('click', decrease);
+resetButton.addEventListener('click', reset);
+increaseButton.addEventListener('click', increase);
